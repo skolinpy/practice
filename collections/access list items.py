e716f3e1ea2218fdec14b9618adcf36b0dc52d17
skolinpy <skolinpy@gmail.com>
2024-02-06 23:26:00 +0300

    доступ к элементам списка

---
+++ collections/access list items.py
@@ -0,0 +1,12 @@
+list1=[1,5,7,'dog',85]
+print(list1[2])
+print(list1[-1])
+print(list1[2:4])
+print(list1[:2])
+print(list1[3:])
+print(list1[0],list1[4])
+if 7 in list1:
+    print("7 - счастливое число")
+
+
+
